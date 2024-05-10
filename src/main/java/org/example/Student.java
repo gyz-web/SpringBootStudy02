fd0a49c0991ecc2b1abb623fbcbbffe8161dd381
guoyongzhuang <1820103130@qq.com>
2024-05-11 00:37:17 +0800

    JSR-303

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -4,13 +4,19 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Email;
 
 @Data
 @Component
+@Validated
 @ConfigurationProperties(prefix = "student")
 public class Student {
 //    @Value("nihao")
     public String name;
+    @Email
+    public String mail;
 //    @Value("18")
     public Integer age;
 }
